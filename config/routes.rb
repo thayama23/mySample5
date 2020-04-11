0e7f8a4f15c64394aa1c1b1336f4ca00726fce31
tetsuhiro.hayama <tetsuhiro.hayama@gmail.com>
2020-04-11 10:00:31 +0900

    firstCommit prior to association

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'blogs/index'
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
